8724e7fc8c1fab380908830a97af6b07421a2dde
shivamXP <shivamp49@yahoo.com>
2018-03-01 14:54:27 -0500

    Delete classLoad.sql

--- classLoad.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA LOCAL INFILE 'dater.csv' 
-INTO TABLE classes  
-    FIELDS TERMINATED BY ', ' 
-           OPTIONALLY ENCLOSED BY '"'
-    LINES  TERMINATED BY '\n' -- or \r\n
-(prefix, num, title, description, prerequisites, department);
