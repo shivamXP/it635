d86b950b31ae6a81bdbc7a8dd75947cd951e0c71
shivamXP <shivamp49@yahoo.com>
2018-03-01 14:54:05 -0500

    Delete authentication.sql

--- authentication.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE if exists users;
-create table users (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	username VARCHAR(16),
-	displayName varchar(255),
-	password varchar(255),
-	permissions varchar(255)
-);
